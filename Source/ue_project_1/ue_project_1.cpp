e71af8b8534db5cc7649b571b328da14cd10055a
pabloinigoblasco <pibgeus@gmail.com>
2023-08-24 06:08:49 -0700

    Initial clone with Office Environment

---
+++ Source/ue_project_1/ue_project_1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ue_project_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ue_project_1, "ue_project_1" );
---
+++ update_project_files.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -z "${UE5_DIR}" ]; then
+        printf "Please set UE5_DIR to path of UE5 UnrealEngine's folder\n"
+        exit 1
+fi
+
+GENERATOR_COMMAND="${UE5_DIR}/Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh"
+
+
+(exec "$GENERATOR_COMMAND" -project="${PWD}/ue_project_1.uproject" -game)
